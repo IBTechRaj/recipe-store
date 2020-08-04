b29503f338b44c697eb796dd7ab843635d3277dd
ibtechraj <raj_shk@rediffmail.com>
2020-08-04 16:30:37 +0530

    merged feature-branch into development

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-// import ReactTestUtils from 'react-dom/test-utils'; // ES6
-import App from '../components/App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../components/App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const component = renderer.create(<App />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
